943111566fe2922c2fff83a2ebf1d8e422658513
gaurav459433 <gaurav459433@gmail.com>
2023-02-24 16:52:21 +0530

    Added: Code For Repeat Till The Player Reaches Winning Position 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -6,30 +6,35 @@ public class SnakeLadder {
         System.out.println();
         int singlePlayer = 0;
         System.out.println("Starting Position Of Single Player is :" + singlePlayer);
-        int dieCheck = (int) Math.floor(Math.random() * 10) % 6 + 1;
-        System.out.println("Player Rolls The Die & Get The Number :" + dieCheck);
-        int optionCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        switch (optionCheck) {
-            case 0:
-                singlePlayer += dieCheck;
-                System.out.println("Player Gets a Ladder & Player Moves ahead By Number Of Position :" + singlePlayer);
+        while (singlePlayer != 100) {
+            int dieCheck = (int) Math.floor(Math.random() * 10) % 6 + 1;
+            System.out.println("Player Rolls The Die & Get The Number :" + dieCheck);
+            int optionCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-                System.out.println("Player Current Position :" + singlePlayer);
-                break;
+            switch (optionCheck) {
+                case 0:
+                    singlePlayer += dieCheck;
+                    System.out.println("Player Gets a Ladder & Player Moves ahead By Number Of Position :" + dieCheck);
 
-            case 1:
-                singlePlayer -= dieCheck;
-                System.out.println("Player Bites By Snake & Player Moves Behind By Number Of Position :" + singlePlayer);
+                    System.out.println("Player Current Position :" + singlePlayer);
+                    break;
 
-                System.out.println("Player Current Position :" + singlePlayer);
-                break;
+                case 1:
+                    singlePlayer -= dieCheck;
+                    System.out.println("Player Bites By Snake & Player Moves Behind By Number Of Position :" + dieCheck);
 
-            case 2:
-                singlePlayer += 0;
-                System.out.println("No Play - Player Stay At Same Position");
-                System.out.println("Player Current Position :" + singlePlayer);
-                break;
+                    System.out.println("Player Current Position :" + singlePlayer);
+                    break;
+
+                case 2:
+                    singlePlayer += 0;
+                    System.out.println("No Play - Player Stay At Same Position");
+                    System.out.println("Player Current Position :" + singlePlayer);
+                    break;
+            }
         }
+        System.out.println();
+        System.out.println("Player Reaches position: " +singlePlayer);
     }
 }
