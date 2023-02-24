c345487aa51661cea0fcf8aa6d4b98ef60ca04f5
gaurav459433 <gaurav459433@gmail.com>
2023-02-24 12:24:34 +0530

    Added: Code For Player Starting Position (0)

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,5 +3,8 @@ package com.bridgelabz.assignmentsrfp259.day04.snakeladder;
 public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome To Snake & Ladder Simulator");
+        System.out.println();
+        int singlePlayer = 0;
+        System.out.println("Starting Position Of Single Player is :" +singlePlayer);
     }
 }
