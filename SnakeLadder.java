a598773f4c249806c596d4e222e833ba61e1f732
gaurav459433 <gaurav459433@gmail.com>
2023-02-24 13:20:48 +0530

    Added: Code For Player Check For NoPlay, Ladder & Snake

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,8 +5,31 @@ public class SnakeLadder {
         System.out.println("Welcome To Snake & Ladder Simulator");
         System.out.println();
         int singlePlayer = 0;
-        System.out.println("Starting Position Of Single Player is :" +singlePlayer);
+        System.out.println("Starting Position Of Single Player is :" + singlePlayer);
         int dieCheck = (int) Math.floor(Math.random() * 10) % 6 + 1;
-        System.out.println("Player Rolls The Die & Get The Number :" +dieCheck);
+        System.out.println("Player Rolls The Die & Get The Number :" + dieCheck);
+        int optionCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+        switch (optionCheck) {
+            case 0:
+                singlePlayer += dieCheck;
+                System.out.println("Player Gets a Ladder & Player Moves ahead By Number Of Position :" + singlePlayer);
+
+                System.out.println("Player Current Position :" + singlePlayer);
+                break;
+
+            case 1:
+                singlePlayer -= dieCheck;
+                System.out.println("Player Bites By Snake & Player Moves Behind By Number Of Position :" + singlePlayer);
+
+                System.out.println("Player Current Position :" + singlePlayer);
+                break;
+
+            case 2:
+                singlePlayer += 0;
+                System.out.println("No Play - Player Stay At Same Position");
+                System.out.println("Player Current Position :" + singlePlayer);
+                break;
+        }
     }
 }
