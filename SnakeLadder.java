da0ae7b89be74634d0202a8c8a4bcb1ec5a28d8c
gaurav459433 <gaurav459433@gmail.com>
2023-02-24 07:51:41 +0530

    Welcome To Snake & Ladder Simulator

---
+++ SnakeLadder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.assignmentsrfp259.day04.snakeladder;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Snake & Ladder Simulator");
+    }
+}
