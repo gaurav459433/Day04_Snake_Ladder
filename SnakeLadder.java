9cced3ad7250f642350e7a1237e8be2629b61ef3
gaurav459433 <gaurav459433@gmail.com>
2023-02-24 23:56:28 +0530

    Added: Code For No. Of Times Die Rolls For Win & Position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,9 +5,11 @@ public class SnakeLadder {
         System.out.println("Welcome To Snake & Ladder Simulator");
         System.out.println();
         int singlePlayer = 0;
+        int count = 0;
         System.out.println("Starting Position Of Single Player is :" + singlePlayer);
 
         while (singlePlayer != 100) {
+            count++;
             int dieCheck = (int) Math.floor(Math.random() * 10) % 6 + 1;
             System.out.println("Player Rolls The Die & Get The Number :" + dieCheck);
             int optionCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -17,17 +19,18 @@ public class SnakeLadder {
                     singlePlayer += dieCheck;
                     System.out.println("Player Gets a Ladder & Player Moves ahead By Number Of Position :" + dieCheck);
                     if (singlePlayer > 100) {
+
                         singlePlayer = 100;
-                    System.out.println("Player Current Position :" + singlePlayer);
+                        System.out.println("Player Current Position :" + singlePlayer);
                     }
                     break;
 
                 case 1:
                     singlePlayer -= dieCheck;
                     System.out.println("Player Bites By Snake & Player Moves Behind By Number Of Position :" + dieCheck);
-                    if ( singlePlayer < 0) {
+                    if (singlePlayer < 0) {
                         singlePlayer = 0;
-                    System.out.println("Player Current Position :" + singlePlayer);
+                        System.out.println("Player Current Position :" + singlePlayer);
                     }
                     break;
 
@@ -37,8 +40,9 @@ public class SnakeLadder {
                     System.out.println("Player Current Position :" + singlePlayer);
                     break;
             }
+            System.out.println("The Number Required By Player is: " + (100 - singlePlayer));
         }
         System.out.println();
-        System.out.println("Player Reaches position: " +singlePlayer);
+        System.out.println("Number of times player played: " + count);
     }
 }
